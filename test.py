caeb0086ff1e7b5b9e86709d02ab91bb60e41055
John Hill <johnhillescobar@users.noreply.github.com>
2023-05-13 20:46:37 -0500

    Initial  Commit

---
+++ test.py
@@ -0,0 +1,8 @@
+import pandas as pd
+import numpy as np
+import pickle
+
+obj = pd.read_pickle(r'C:\Users\johnh\OneDrive\Documents\MachineLearningProjects\MLEnd2End2\scaling.pkl')
+
+print(obj)
+print(type(obj))
